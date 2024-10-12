8cf98f622956c8b36b556baf3c2c9a4e46448972
boshranofal <boshranofal66@gmail.com>
2024-10-12 15:25:36 +0300

    feat: working on the news app

--- lib/core/utils/app_constant.dart
+++ lib/core/utils/app_constant.dart
@@ -1,4 +1,6 @@
 class AppConstant {
   static const String appName = 'News App';
   static const String appKey = '9a2d07d63dfc4eb5a3c4204a9ece7b72';
+  static const String baseUrl = 'https://newsapi.org/';
+  static const String topHeadlines = '/v2/top-headlines';
 }
