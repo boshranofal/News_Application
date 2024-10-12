d4ec48e2d85111f5467fc161b81ae11085287046
boshranofal <boshranofal66@gmail.com>
2024-10-12 15:24:55 +0300

    feat: working on the news app

--- lib/core/utils/routes/app_router.dart
+++ lib/core/utils/routes/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:news_application/features/home_page.dart';
-import 'package:news_application/utils/routes/app_route.dart';
+import 'package:news_application/features/views/home_page.dart';
+import 'package:news_application/core/utils/routes/app_route.dart';
 
 class AppRouter {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
