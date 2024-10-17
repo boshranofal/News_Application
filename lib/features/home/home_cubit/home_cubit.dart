18fbd310c97ff264521c581a2877c75ef683776a
boshranofal <boshranofal66@gmail.com>
2024-10-17 20:29:05 +0300

    Add news ,route and page, and implement widget

--- lib/features/home/home_cubit/home_cubit.dart
+++ lib/features/home/home_cubit/home_cubit.dart
@@ -1,8 +1,53 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+
+import 'package:news_application/features/home/home_model/top_headlines_body.dart';
+import 'package:news_application/features/home/home_model/top_headlines_response.dart';
+import 'package:news_application/features/home/services/home_services.dart';
 
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
+  final homeServices = HomeServices();
+
+  Future<void> getHomeCarusel() async {
+    emit(HomeCarouselLoading());
+    try {
+      final body = TopHeadlinesBody(
+          q: 'bitcoin',
+          //country: 'us',
+          //category: 'technology',
+          pageSize: 6);
+      final topHeadLines = await homeServices.getTopheadlines(body);
+      emit(
+        HomeCarouselLoaded(topHeadLines.articles ?? []),
+      );
+    } catch (e) {
+      emit(
+        HomeCarouselError(
+          e.toString(),
+        ),
+      );
+    }
+  }
+
+  Future<void> getHomeList() async {
+    emit(HomeListLoading());
+    try {
+      final body = TopHeadlinesBody(
+          //country: 'us',
+          q: 'bitcoin',
+          pageSize: 10);
+      final topHeadLines = await homeServices.getTopheadlines(body);
+      emit(
+        HomeListLoaded(topHeadLines.articles ?? []),
+      );
+    } catch (e) {
+      emit(
+        HomeListError(
+          e.toString(),
+        ),
+      );
+    }
+  }
 }
--- lib/features/home/home_cubit/home_state.dart
+++ lib/features/home/home_cubit/home_state.dart
@@ -1,6 +1,33 @@
 part of 'home_cubit.dart';
 
-@immutable
 sealed class HomeState {}
 
 final class HomeInitial extends HomeState {}
+
+final class HomeCarouselLoading extends HomeState {}
+
+final class HomeCarouselLoaded extends HomeState {
+  final List<Article> articles;
+
+  HomeCarouselLoaded(this.articles);
+}
+
+final class HomeCarouselError extends HomeState {
+  final String message;
+
+  HomeCarouselError(this.message);
+}
+
+final class HomeListLoading extends HomeState {}
+
+final class HomeListLoaded extends HomeState {
+  final List<Article> articles;
+
+  HomeListLoaded(this.articles);
+}
+
+final class HomeListError extends HomeState {
+  final String message;
+
+  HomeListError(this.message);
+}
